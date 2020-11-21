2af4de3ab752dcc719f09c047200fce78a5d4a37
Sayeem <49128232+Amanullah-Sayeem@users.noreply.github.com>
2020-11-22 00:49:25 +0600

    Update settings.py

--- GraySpace/settings.py
+++ GraySpace/settings.py
@@ -120,6 +120,9 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
     )
 }
 
